305ff51c599a818aa039b69d97a217133aac1f06
unknown <chris.d.reid@gmail.com>
2020-11-20 19:15:07 -0800

    added link to odrive robotics forum where the sample was created

--- src/esp32_odrive_sample.cpp
+++ src/esp32_odrive_sample.cpp
@@ -3,6 +3,7 @@
  * ** https://hackaday.com/2017/08/17/secret-serial-port-for-arduinoesp32/
  * ** https://youtu.be/GwShqW39jlE
  * 
+ * ** Link of post on odriverobotics forum: https://discourse.odriverobotics.com/t/esp32-odrive-communication-solved/3422/4
  * Much of the code is from the Example file in ODriveArduino Examples: ODriveArduinoTest
 
  PIN | ESP32 | ODrive
